59f50a1def3a52fdd35a6c1577e3272371cb8c7f
DepressedCookie <vipc.1224@live.ca>
2020-11-21 18:29:26 -0500

    Added audio manager

---
+++ Assets/Scripts/World/Script_AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_AudioManager : MonoBehaviour
+{
+    public string StartMap;
+    public List<string> MapName;
+    public List<AudioSource> Audio;
+    private Dictionary<string, AudioSource> AudioMap = new Dictionary<string, AudioSource>();
+
+    private void Awake()
+    {
+        int count = FindObjectsOfType<Script_AudioManager>().Length;
+        if (count != 1) { Destroy(this); } else { DontDestroyOnLoad(this);}
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        string map = SceneManager.GetActiveScene().name;
+        bool safe = AudioMap.ContainsKey(map) && !AudioMap[map].isPlaying;
+        if (safe)
+        {
+            foreach (AudioSource au in Audio) { au.Stop(); }
+            AudioMap[map].Play();
+        }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < Audio.Count; i++) { AudioMap.Add(MapName[i], Audio[i]); }
+        if(AudioMap.ContainsKey(StartMap)) { AudioMap[StartMap].Play(); }
+    }
+}
